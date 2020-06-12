a4fb6ced4a9a130945d7579373fe0d69aa7c2e1b
jonbos-palmer <jbos@thepalmergroup.com>
2020-06-12 09:15:35 -0500

    adds abc with direct inheritance from ABC

--- src/deadlined_reminders.py
+++ src/deadlined_reminders.py
@@ -1,5 +1,6 @@
-from abc import ABCMeta, abstractmethod
+from abc import ABCMeta, abstractmethod, ABC
 from collections.abc import Iterable
+from dateutil.parser import parse
 
 class DeadlinedMetaReminder(Iterable, metaclass=ABCMeta):
 
@@ -7,3 +8,9 @@ class DeadlinedMetaReminder(Iterable, metaclass=ABCMeta):
     def is_due(self):
         pass
 
+
+class DeadlinedReminder(ABC, Iterable):
+    
+    @abstractmethod
+    def is_due(self):
+        pass
